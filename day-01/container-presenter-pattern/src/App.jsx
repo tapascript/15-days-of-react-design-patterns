d5f86bc65f8d472e2229efa3cd863b739abef007
Tapas Adhikary <tapas.adhikary@gmail.com>
2025-09-18 16:01:35 +0530

    (feat) Added the messy way of handling user profile

---
+++ day-01/container-presenter-pattern/src/App.jsx
@@ -0,0 +1,11 @@
+import UserProfile from "./components/messy-way/UserProfile";
+import "./App.css"
+function App() {
+  return (
+    <div>
+     <UserProfile userId={1}/>
+    </div>
+  );
+}
+
+export default App;
