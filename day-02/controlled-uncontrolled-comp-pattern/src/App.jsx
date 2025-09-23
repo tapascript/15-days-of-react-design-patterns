7242d9a2c1dd18afe37abad1772a9f506c886000
Tapas Adhikary <tapas.adhikary@gmail.com>
2025-09-23 14:51:09 +0530

    (day 02) controlled uncontrolled initial sekeleton added a counter component

---
+++ day-02/controlled-uncontrolled-comp-pattern/src/App.jsx
@@ -0,0 +1,11 @@
+import Counter from "./state-ref/components/Counter";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center">
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
