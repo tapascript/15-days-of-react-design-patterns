1838d6b37dc926038a37efb9bedcf31b1bcce704
Tapas Adhikary <tapas.adhikary@gmail.com>
2025-09-23 15:16:13 +0530

    (day 02) Added more demo for state and refs

--- day-02/controlled-uncontrolled-comp-pattern/src/App.jsx
+++ day-02/controlled-uncontrolled-comp-pattern/src/App.jsx
@@ -1,9 +1,13 @@
 import Counter from "./state-ref/components/Counter";
+import AutoFocusInput from "./state-ref/components/AutoFocusInput";
+import CounterWithRef from "./state-ref/components/CounterWithRef";
 
 function App() {
   return (
     <div className="flex flex-col items-center">
       <Counter />
+      <AutoFocusInput />
+      <CounterWithRef />
     </div>
   );
 }
