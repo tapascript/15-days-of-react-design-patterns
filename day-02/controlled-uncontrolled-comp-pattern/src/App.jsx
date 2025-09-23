3799369e93ee25f728557c8fc4e7f9c7972d9b30
Tapas Adhikary <tapas.adhikary@gmail.com>
2025-09-23 15:30:49 +0530

    (day 02) Controlled vs uncontrolled - Added the messy code example

--- day-02/controlled-uncontrolled-comp-pattern/src/App.jsx
+++ day-02/controlled-uncontrolled-comp-pattern/src/App.jsx
@@ -2,12 +2,15 @@ import Counter from "./state-ref/components/Counter";
 import AutoFocusInput from "./state-ref/components/AutoFocusInput";
 import CounterWithRef from "./state-ref/components/CounterWithRef";
 
+import FeedbackForm from "./messy/FeedbackForm";
+
 function App() {
   return (
     <div className="flex flex-col items-center">
       <Counter />
       <AutoFocusInput />
       <CounterWithRef />
+      <FeedbackForm />
     </div>
   );
 }
