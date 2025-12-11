95e31bc008ffbcd71566592538f0fdcdab409a6c
Tapas Adhikary <tapas.adhikary@gmail.com>
2025-12-11 15:20:37 +0530

    (Day 12) Slot Pattern Default Slot Demo

---
+++ day-12/slot-pattern/src/default-slot/DefaultSlotDemo.jsx
@@ -0,0 +1,12 @@
+import DefaultSlotCard from "./DefaultSlotCard";
+
+const DefaultSlotDemo = () => {
+    return (
+        <DefaultSlotCard>
+            <h3>Title</h3>
+            <p>Body goes here.</p>
+        </DefaultSlotCard>
+    );
+};
+
+export default DefaultSlotDemo;
